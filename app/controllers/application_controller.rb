e55bf412e981598da9949df092f74f080078d828
nicolasUribeV <nikolas_1414@hotrmail.com>
2014-06-23 23:07:16 -0400

    Registro Usuarios y Cambios en Diseño

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,13 @@ class ApplicationController < ActionController::Base
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :email
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
   helper_method :resource, :resource_name, :devise_mapping
 end
