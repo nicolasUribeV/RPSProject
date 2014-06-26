bdb48ac13d5bece25b7093fc984ad77994c16f87
dmora91 <d.mora91@hotmail.com>
2014-06-26 02:26:05 -0400

    The Dropdown Club
    agrege algunos dropdown

--- app/models/arrendatario.rb
+++ app/models/arrendatario.rb
@@ -2,4 +2,5 @@ class Arrendatario < ActiveRecord::Base
   belongs_to :Usuario
   has_many :ContratoArriendos
   has_many :Solicituds
+  validates_uniqueness_of :Usuario_id
 end
--- app/models/ubicacion.rb
+++ app/models/ubicacion.rb
@@ -1,5 +1,5 @@
 class Ubicacion < ActiveRecord::Base
-  has_many :Ubicacions
+  has_many :Ubicacions, dependent: :destroy
   belongs_to :Ubicacion
   has_many :Propiedads
 end
