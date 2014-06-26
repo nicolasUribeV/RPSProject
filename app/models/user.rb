42e92131ac75f30d28e2d583ffc702f2c5226965
nicolasUribeV <nikolas_1414@hotrmail.com>
2014-06-26 02:03:00 -0400

    Validación RUT

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :username, rut: true, message: "RUT Invalido"
+  validates_uniqueness_of :username, message: "RUT ya se encuentra en el sistema"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
