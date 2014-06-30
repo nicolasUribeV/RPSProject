08c01c0a00d6c239a0ff868b8d607d6bbe015ae6
eliasplease <elias.sobarzo@usach.cl>
2014-06-29 21:50:44 -0400

    validación ingreso item y exp. reg. rut

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ActiveRecord::Base
 	has_many :InventarioPropiedads
 	has_many :Propiedads, :through => :InventarioPropiedads, :dependent => :destroy
+
+	validates :NombreItem, :presence =>{:message => "No puede quedar en blanco"}, confirmation: true 
 end
