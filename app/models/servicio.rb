ea71bfbb9ca4bad2454e3e93fcc9250770c35859
eliasplease <elias.sobarzo@usach.cl>
2014-06-29 21:11:11 -0400

    scroll tablas y validacion en nombre servicio

--- app/models/servicio.rb
+++ app/models/servicio.rb
@@ -1,4 +1,6 @@
 class Servicio < ActiveRecord::Base
   has_many :ServicioContratados
   has_many :ContratoPropiedads, :through => :ServicioContratados, :dependent => :destroy
+
+  validates :NombreServicio, :presence =>{:message => "No puede quedar en blanco"}, confirmation: true 
 end
